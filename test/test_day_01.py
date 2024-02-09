b6a5444e89dd3a575fec7b84d4c183026c038ac7
Eduardo Ellery <eduellery@gmail.com>
2024-02-09 19:25:57 +0100

    Refactor Day 01

--- test/test_day_01.py
+++ test/test_day_01.py
@@ -3,9 +3,10 @@ from typing import List
 import pytest
 from aoc.day_01 import Day01
 
-from resources import read_as_text
+from resources import read_as_text, file_exists
 
-aoc_input = Day01(read_as_text('test/day01.in').split(', '))
+local_test =  file_exists('test/day01.in')
+aoc_input = Day01(read_as_text('test/day01.in').split(', ')) if local_test else None
 
 
 @pytest.mark.parametrize("test_input, expected", [
@@ -17,6 +18,7 @@ def test_solve_1_examples(test_input: List[str], expected: int):
     assert Day01(test_input).solve1() == expected
 
 
+@pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_1_input():
     assert aoc_input.solve1() == 287
 
@@ -25,5 +27,6 @@ def test_solve_2_examples():
     assert Day01(['R8', 'R4', 'R4', 'R8']).solve2() == 4
 
 
+@pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_2_input():
     assert aoc_input.solve2() == 133
