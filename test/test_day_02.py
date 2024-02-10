a66e54bfbd3d349930a82381c368f0b6add5770c
Eduardo Ellery <eduellery@gmail.com>
2024-02-10 07:29:56 +0100

    Refactor Day 02

--- test/test_day_02.py
+++ test/test_day_02.py
@@ -1,7 +1,9 @@
+import pytest
 from aoc.day_02 import Day02
-from resources import read_as_string_list
+from resources import read_as_string_list, file_exists
 
-aoc_input = Day02(read_as_string_list('test/day02.in'))
+local_test =  file_exists('test/day02.in')
+aoc_input = Day02(read_as_string_list('test/day02.in')) if local_test else None
 test_input = Day02(['ULL', 'RRDDD', 'LURDL', 'UUUUD'])
 
 
@@ -9,6 +11,7 @@ def test_solve_1_examples():
     assert test_input.solve1() == '1985'
 
 
+@pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_1_input():
     assert aoc_input.solve1() == '99332'
 
@@ -17,5 +20,6 @@ def test_solve_2_examples():
     assert test_input.solve2() == '5DB3'
 
 
+@pytest.mark.skipif(not local_test, reason = 'Input files can not be shared')
 def test_solve_2_input():
     assert aoc_input.solve2() == 'DD483'
