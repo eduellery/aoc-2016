d4eb562bc3295308438b5b67ea4fd7974400cf5c
Eduardo Ellery <eduellery@gmail.com>
2025-01-09 21:05:35 +0100

    2016 - Day 15 - Part 1

--- src/day_15.py
+++ src/day_15.py
@@ -1,16 +1,23 @@
-import functools
-from hashlib import md5
-from re import compile, findall
+from re import findall
 
+
+def find_alignment(factors):
+    t = 0
+    while True:
+        if all(t % mod == remainder for mod, remainder in factors):
+            return t
+        t += 1
 
 class Day15:
     def __init__(self, values: list[str]):
-        print("Print values for Day 15")
-        for value in values:
-            print(value)
+        self.factors = []
+        for i, value in enumerate(values, start=1):
+            _, mod, _, x = map(int, findall(r"\b\d+\b", value))
+            x = (mod - x - i) % mod
+            self.factors.append((mod, x))
 
     def solve1(self) -> int:
-        return 1
+        return find_alignment(self.factors)
 
     def solve2(self) -> int:
         return 2
--- test/test_day_15.py
+++ test/test_day_15.py
@@ -9,12 +9,12 @@ test_input = Day15(read_as_string_list("res/day15.example"))
 
 
 def test_solve_1_examples():
-    assert test_input.solve1() == 1
+    assert test_input.solve1() == 5
 
 
 @pytest.mark.skipif(not local_test, reason="Input files can not be shared")
 def test_solve_1_input():
-    assert aoc_input.solve1() == 1
+    assert aoc_input.solve1() == 122318
 
 
 def test_solve_2_examples():
