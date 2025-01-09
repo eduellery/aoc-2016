0fa7466f6af02185f49427e57d93d93327695572
Eduardo Ellery <eduellery@gmail.com>
2025-01-09 05:32:18 +0100

    2016 - Day 15 - skeleton

---
+++ src/day_15.py
@@ -0,0 +1,16 @@
+import functools
+from hashlib import md5
+from re import compile, findall
+
+
+class Day15:
+    def __init__(self, values: list[str]):
+        print("Print values for Day 15")
+        for value in values:
+            print(value)
+
+    def solve1(self) -> int:
+        return 1
+
+    def solve2(self) -> int:
+        return 2
---
+++ test/test_day_15.py
@@ -0,0 +1,26 @@
+import pytest
+from resources import file_exists, read_as_string_list
+
+from day_15 import Day15
+
+local_test = file_exists("res/day15.in")
+aoc_input = Day15(read_as_string_list("res/day15.in")) if local_test else None
+test_input = Day15(read_as_string_list("res/day15.example"))
+
+
+def test_solve_1_examples():
+    assert test_input.solve1() == 1
+
+
+@pytest.mark.skipif(not local_test, reason="Input files can not be shared")
+def test_solve_1_input():
+    assert aoc_input.solve1() == 1
+
+
+def test_solve_2_examples():
+    assert test_input.solve2() == 2
+
+
+@pytest.mark.skipif(not local_test, reason="Input files can not be shared")
+def test_solve_2_input():
+    assert aoc_input.solve2() == 2
