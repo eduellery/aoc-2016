d459afdb12e329060990ddcba1d27589d86c54ba
Eduardo Ellery <eduellery@gmail.com>
2025-01-14 18:24:09 +0100

    2016 - Day 16 - Part 2

--- src/day_16.py
+++ src/day_16.py
@@ -1,34 +1,27 @@
-def reverse(value: str) -> str:
-    return value[::-1]
-
-def invert(value: str) -> str:
-    return "".join('0' if c == '1' else '1' for c in value)
+def invert_reverse(value: str) -> str:
+    return "".join('0' if c == '1' else '1' for c in value[::-1])
 
 def fill(value: str, size: int) -> str:
     result = value
     while (len(result) < size):
-        result = result + "0" + invert(reverse(result))
+        result = result + "0" + invert_reverse(result)
     return result[:size]
 
 def reduce(value: str) -> str:
-    result = ""
+    result = []
     for i in range(0, len(value), 2):
-        result += "1" if value[i:i+2] in ["00", "11"] else "0"
-    return result
+        result.append("1" if value[i] == value[i+1] else "0")
+    return "".join(result)
 
 def checksum(value: str) -> str:
-    result = value
+    result = reduce(value)
     while len(result) % 2 == 0:
         result = reduce(result)
     return result
 
 class Day16:
-    def __init__(self, seed: str, size: int = 20):
+    def __init__(self, seed: str):
         self.seed = seed
-        self.size = size
 
-    def solve1(self) -> str:
-        return checksum(fill(self.seed, self.size))
-
-    def solve2(self) -> int:
-        return 2
+    def solve(self, size: int = 20) -> str:
+        return checksum(fill(self.seed, size))
--- test/test_day_16.py
+++ test/test_day_16.py
@@ -5,13 +5,18 @@ from resources import file_exists, read_as_string
 from day_16 import Day16
 
 local_test = file_exists("res/day16.in")
-aoc_input = Day16(read_as_string("res/day16.in"), 272) if local_test else None
+aoc_input = Day16(read_as_string("res/day16.in")) if local_test else None
 
 
 def test_solve_1_examples():
-    assert Day16("10000").solve1() == "01100"
+    assert Day16("10000").solve() == "01100"
 
 
 @pytest.mark.skipif(not local_test, reason="Input files can not be shared")
 def test_solve_1_input():
-    assert aoc_input.solve1() == "10010010110011010"
+    assert aoc_input.solve(272) == "10010010110011010"
+
+
+@pytest.mark.skipif(not local_test, reason="Input files can not be shared")
+def test_solve_2_input():
+    assert aoc_input.solve(35651584) == "01010100101011100"
