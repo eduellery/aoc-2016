388b729b87d520344b7341655cea6651fdd0c94d
Eduardo Ellery <eduellery@gmail.com>
2025-01-17 18:36:34 +0100

    2016 - Day 18 - Part 1

---
+++ src/day_18.py
@@ -0,0 +1,22 @@
+def is_safe(left: bool, right: bool) -> bool:
+    return not (left ^ right)
+
+def next_row(row: list[bool]) -> list[bool]:
+    new_row = [True]
+    for i in range(1, len(row)-1):
+        new_row.append(is_safe(row[i-1], row[i+1]))
+    new_row.append(True)
+    return new_row
+
+class Day18:
+    def __init__(self, value: str):
+        self.traps = [c == '.' for c in value]
+        self.traps = [True] + self.traps + [True]
+
+    def solve1(self, rounds: int = 40) -> str:
+        row = self.traps
+        count = sum(row) - 2
+        for _ in range(1, rounds):
+            row = next_row(row)
+            count += sum(row) - 2
+        return count
---
+++ test/test_day_18.py
@@ -0,0 +1,24 @@
+import pytest
+from resources import file_exists, read_as_string
+
+from day_18 import Day18
+
+local_test = file_exists("res/day18.in")
+aoc_input = Day18(read_as_string("res/day18.in")) if local_test else None
+
+
+@pytest.mark.parametrize(
+    "test_input, rounds, expected",
+    [
+        ("..^^.", 3, 6),
+        (".^^.^.^^^^", 10, 38),
+    ],
+)
+def test_solve_1_examples(test_input: str, rounds: int, expected: int):
+    assert Day18(test_input).solve1(rounds) == expected
+
+
+@pytest.mark.skipif(not local_test, reason="Input files can not be shared")
+def test_solve_1_input():
+    assert aoc_input.solve1() == 2013
+
